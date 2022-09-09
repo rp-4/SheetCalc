232cfbaa321337929894fc6f2a2ab010ead531bb
Rinkesh <rinkeshpatel44@gmail.com>
2022-09-09 19:59:23 -0400

    Finished UI Finished User interface, validate entry whether it is number or not.

--- Equal_Size_Remnants.py
+++ Equal_Size_Remnants.py
@@ -3,26 +3,40 @@ from tkinter import ttk
 from tkinter import messagebox
 
 
+def numCheck():
+    number = list(mainText_entry.get())
+    for i in range(0,len(number)):
+        if number[i].isdigit() or number[i] == ".":
+            #print(number[i])
+            return True
+        else:
+            return False
+        
 
 def result():
-    size = sheet.get()
-    if size.isdigit():
-        size = float(sheet.get())
-    else:
+    if not numCheck():
         messagebox.showerror('Wait What?','Please enter number.')
+        clear()
+        length = 0
+        finalResult_label.config(text="No Number")
         return
+    else:
+        length = float(mainText_entry.get())
     
-    length = float(mainText_entry.get())
+    size = float(sheet.get())
     result = (size - (length/2))
     result = format(result,'.4f')
     finalResult_label.config(text=result)
 
+def clear():
+    mainText_entry.delete(0,END)
     
 # defining window
 root = Tk()
 root.title("Equal Size Remnants Calc V0.1")
-sq = 240
-root.geometry(f'{sq}x{sq}')
+w = 240
+h = 250
+root.geometry(f'{w}x{h}')
 root.resizable(False, False)
 mainframe = ttk.Frame(root, padding = "3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
@@ -53,15 +67,17 @@ sheet.set("120")
 #Enter length
 
 mainText = StringVar()
-mainText_entry = ttk.Entry(mainframe, width=18, textvariable=mainText)
+mainText_entry = ttk.Entry(mainframe, width=10, font=('Micross',18), textvariable=mainText)
 mainText_entry.grid(row=3, column=0, columnspan=2, pady=10)
 
 
 
-# Add, delete, update and copy buttons
-ttk.Button(mainframe, text = "Result",command = result).grid(row=4, column=0, columnspan=2, pady=5)
+# Button to get Result
+ttk.Button(mainframe, text = "Result", command = result).grid(row=4, column=0, pady=5)
 
+#button to clear
+ttk.Button(mainframe, text = "Clear",command = clear).grid(row=4, column=1,  pady=5)
 
 madeBy = Label(root, text="Developed by r Patel with 💙 !!")
-madeBy.grid(row=5, column=0, columnspan=4, pady=5)
+madeBy.grid(row=5, column=0, columnspan=4, pady=10)
 root.mainloop()
